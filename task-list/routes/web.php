4d371df9aadd6b3c7930301208e534305cd08691
Breno <brenolimanf@hotmail.com>
2025-10-24 20:09:15 -0300

    Studying Routes

---
+++ task-list/routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return 'Main Page';
+});
+
+Route::get('/hello', function () {
+    return 'Hello';
+});
+
+Route::get('/greet/{name}', function ($name) {
+    return "Hello, $name!";
+});
+
+//GET
+//POST
+//PUT
+//DELETE

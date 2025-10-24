ccfa902c9275b06e651deef226821a3932c51365
Breno <brenolimanf@hotmail.com>
2025-10-24 20:25:30 -0300

    Routes

--- task-list/routes/web.php
+++ task-list/routes/web.php
@@ -8,12 +8,19 @@ Route::get('/', function () {
 
 Route::get('/hello', function () {
     return 'Hello';
+})->name('hello');
+
+Route::get('/hallo', function () {
+    return redirect()->route('hello');
 });
 
 Route::get('/greet/{name}', function ($name) {
     return "Hello, $name!";
 });
 
+Route::fallback(function (){
+    return 'still got somewhere!';
+});
 //GET
 //POST
 //PUT

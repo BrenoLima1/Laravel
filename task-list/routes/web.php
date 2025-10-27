c83fd8906bae910f496c26a0cca479aa4478b99e
Breno <brenolimanf@hotmail.com>
2025-10-26 21:59:19 -0300

    form created

--- task-list/routes/web.php
+++ task-list/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Response;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
@@ -79,9 +80,16 @@ Route::get('/tasks', function () use ($tasks) {
     ]);
 })->name('tasks.index');
 
+Route::view('/tasks/create', 'create'
+)->name('tasks.create');
+
 Route::get('/tasks/{id}', function ($id)  {
-    return view('show', ['task' => \App\Models\Task::find($id)]);
+    return view('show', ['task' => \App\Models\Task::findOrFail($id)]);
 })->name('tasks.show');
+
+Route::post('/tasks', function (Request $request) {
+    dd($request->all());
+})->name('tasks.store');
 
 // Route::get('/xxx', function () {
 //     return 'Hello';

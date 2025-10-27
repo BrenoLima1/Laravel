0193d41a76e078aeaf51f4a65a906dc3e67878e9
Breno <brenolimanf@hotmail.com>
2025-10-26 22:28:08 -0300

    web file updated

--- task-list/app/Models/Task.php
+++ task-list/app/Models/Task.php
@@ -5,7 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
 class Task extends Model
 {
-    use HasFactory;
+    protected $fillable = ['title', 'description', 'long_description'];
 }
--- task-list/routes/web.php
+++ task-list/routes/web.php
@@ -1,128 +1,40 @@
 <?php
 
-use Illuminate\Http\Response;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-class Task
-{
-    public function __construct(
-        public int $id,
-        public string $title,
-        public string $description,
-        public ?string $long_description,
-        public bool $completed,
-        public string $created_at,
-        public string $updated_at
-    ) {
-    }
-}
-
-$tasks = [
-    new Task(
-        1,
-        'Buy groceries',
-        'Task 1 description',
-        'Task 1 long description',
-        false,
-        '2023-03-01 12:00:00',
-        '2023-03-01 12:00:00'
-    ),
-    new Task(
-        2,
-        'Sell old stuff',
-        'Task 2 description',
-        null,
-        false,
-        '2023-03-02 12:00:00',
-        '2023-03-02 12:00:00'
-    ),
-    new Task(
-        3,
-        'Learn programming',
-        'Task 3 description',
-        'Task 3 long description',
-        true,
-        '2023-03-03 12:00:00',
-        '2023-03-03 12:00:00'
-    ),
-    new Task(
-        4,
-        'Take dogs for a walk',
-        'Task 4 description',
-        null,
-        false,
-        '2023-03-04 12:00:00',
-        '2023-03-04 12:00:00'
-    ),
-];
-
+use App\Models\Task;
 
 Route::get('/', function () {
     return redirect()->route('tasks.index');
 });
 
-Route::get('/tasks', function () use ($tasks) {
-    return view('index', [
-        'tasks' => $tasks
-    ]);
+Route::get('/tasks', function () {
+    $tasks = Task::all(); // busca do banco
+    return view('index', ['tasks' => $tasks]);
 })->name('tasks.index');
 
-Route::view('/tasks/create', 'create'
-)->name('tasks.create');
+Route::view('/tasks/create', 'create')->name('tasks.create');
 
-Route::get('/tasks/{id}', function ($id)  {
-    return view('show', ['task' => \App\Models\Task::findOrFail($id)]);
+Route::get('/tasks/{id}', function ($id) {
+    return view('show', ['task' => Task::findOrFail($id)]);
 })->name('tasks.show');
 
 Route::post('/tasks', function (Request $request) {
-    dd($request->all());
+    $data = $request->validate([
+        'title' => 'required|max:255',
+        'description' => 'required',
+        'long_description' => 'nullable',
+    ]);
+
+    $task = new Task($data);
+    $task->completed = false;
+    $task->save();
+
+    return redirect()->route('tasks.index');
 })->name('tasks.store');
 
-// Route::get('/xxx', function () {
-//     return 'Hello';
-// })->name('hello');
+Route::get('/hello', fn() => 'Hello')->name('hello');
+Route::get('/hallo', fn() => redirect()->route('hello'));
+Route::get('/greet/{name}', fn($name) => "Hello, $name!");
 
-// Route::get('/hallo', function () {
-//     return redirect()->route('hello');
-// });
-
-// Route::get('/greet/{name}', function ($name) {
-//     return 'Hello ' . $name . '!';
-// });
-
-Route::fallback(function () {
-    return 'Still got somewhere!';
-});
-
-Route::get('/hello', function () {
-    return 'Hello';
-})->name('hello');
-
-Route::get('/hallo', function () {
-    return redirect()->route('hello');
-});
-
-Route::get('/greet/{name}', function ($name) {
-    return "Hello, $name!";
-});
-
-Route::fallback(function (){
-    return 'still got somewhere!';
-});
-//GET
-//POST
-//PUT
-//DELETE
+Route::fallback(fn() => 'Still got somewhere!');

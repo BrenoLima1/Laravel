649090e1e374a8bb0f75d2d8cb647f477d2ee972
Breno <brenolimanf@hotmail.com>
2025-10-28 22:34:20 -0300

    edition enabled

--- task-list/routes/web.php
+++ task-list/routes/web.php
@@ -30,8 +30,36 @@ Route::post('/tasks', function (Request $request) {
     $task->completed = false;
     $task->save();
 
-    return redirect()->route('tasks.index');
+    return redirect()->route('tasks.show', ['id' => $task->id])
+    ->with('success', 'Task created successfully!');
 })->name('tasks.store');
+
+Route::get('tasks/{id}/edit', function ($id) {
+    return view('edit', ['task' => Task::findOrFail($id)]);
+})->name('tasks.edit');
+
+Route::put('/tasks/{id}', function ($id, Request $request) {
+    $data = $request->validate([
+        'title' => 'required|max:255',
+        'description' => 'required',
+        'long_description' => 'nullable',
+    ]);
+
+    $task = Task::findOrFail($id);
+
+    // aplica os dados validados
+    $task->title = $data['title'];
+    $task->description = $data['description'];
+    $task->long_description = $data['long_description'] ?? null;
+
+    // se quiser resetar o completed sempre
+    $task->completed = false;
+
+    $task->save();
+
+    return redirect()->route('tasks.show', $task->id)
+        ->with('success', 'Task updated successfully!');
+})->name('tasks.update');
 
 Route::get('/hello', fn() => 'Hello')->name('hello');
 Route::get('/hallo', fn() => redirect()->route('hello'));
